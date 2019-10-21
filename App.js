9b6d7dbbc7ed0111374ed7e0250b544092915b03
Ivan Cetta <icetta@gmail.com>
2019-10-21 15:23:59 -0300

    add react-native-maps

--- App.js
+++ App.js
@@ -2,16 +2,54 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-export default function App() {
-  console.log("Provideer ", PROVIDER_GOOGLE);
-  return (
-    <View style={styles.container}>
-      <MapView
-          provider={PROVIDER_GOOGLE}
-          style={{ ...StyleSheet.absoluteFillObject }}
-        />
-    </View>
-  );
+
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
+const LATITUDE = -34.619010;
+const LONGITUDE = -58.516050;
+
+class App extends React.Component  {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+     latitude: LATITUDE,
+     longitude: LONGITUDE,
+     error: null
+    };
+   }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        error: null
+      });
+    },
+    error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000 }
+      );
+  }
+   
+  getMapRegion = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA
+  });
+  // style={{ ...StyleSheet.absoluteFillObject }}
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={{ ...StyleSheet.absoluteFillObject }}
+                provider={PROVIDER_GOOGLE} 
+                region={this.getMapRegion()} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -22,3 +60,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App
